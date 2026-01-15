bd383a99985824f5f4be98ac9419642d8738079c
lianecagara <eckobautista03@gmail.com>
2026-01-15 21:47:54 +0800

    uwu

--- CommandFiles/commands/1_template.js
+++ CommandFiles/commands/1_template.js
@@ -1,15 +1,17 @@
 // @ts-check
 
+import { CassMenu } from "@cass-interact";
+
 // This is a command template!
 // Make a new file, copy+paste, and modify!
 
-export default easyCMD({
-  name: "hello",
-  description: "Greets a user.",
-  title: "💗 Greetings",
-  icon: "💗",
-  category: "Fun",
-  async run({ print, reaction, edit, atReply, userName, getMoney, setMoney }) {
+const cassMenu = new CassMenu();
+
+cassMenu.description("reward", "Claim a random reward.");
+
+cassMenu.option(
+  "reward",
+  async ({ print, reaction, edit, atReply, userName, getMoney, setMoney }) => {
     const money = await getMoney();
     const reward = 5;
 
@@ -23,5 +25,23 @@ export default easyCMD({
     atReply(({ print }) => {
       print("Thanks for replying!");
     });
+  }
+);
+
+cassMenu.description("uwu", "Idk, just say uwu too.");
+
+cassMenu.option("uwu", async ({ reaction, reply }) => {
+  reaction("😅");
+  reply("UwU too!");
+});
+
+export default easyCMD({
+  name: "hello",
+  description: "Greets a user.",
+  title: "💗 Greetings",
+  icon: "💗",
+  category: "Fun",
+  async run({ runContextual }) {
+    await runContextual(cassMenu);
   },
 });
